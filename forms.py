e6b6d289d34d45284b31053e3572e858f1bf8d58
greenruck <drake.northington@gmail.com>
2023-02-14 11:06:06 -0500

    intial

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, InputRequired
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserForm(FlaskForm):
+
+    username =StringField('Username', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[Email()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image URL')
+    bio =  StringField('Your Bio', validators=[Length(max=240)])
+    password = PasswordField('Password', validators=[Length(min=6)])
